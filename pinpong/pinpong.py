0c3b377d6297443a2d8be0ea9d4311b50c8fcf85
NenovAlexsandr <93872785+NenovAlexsandr@users.noreply.github.com>
2021-11-29 16:25:48 +0200

    # там где выдаёт ошибку

--- pinpong/pinpong.py
+++ pinpong/pinpong.py
@@ -1,6 +1,6 @@
 from pygame import *
 from random import randint
-
+from time import time as timer
 
 
 
@@ -39,32 +39,56 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < 500 - 80:
             self.rect.y += self.speed
+
+
+
+
+class Area():
+  def __init__(self, x=0, y=0, width=10, height=10, color=None):
+      self.rect = Rect(x, y, width, height) #прямоугольник
+      self.fill_color = color
+  def color(self, new_color):
+      self.fill_color = new_color
+  def fill(self):
+      draw.rect(window, self.fill_color, self.rect)
+  def collidepoint(self, x, y):
+      return self.rect.collidepoint(x, y)      
+'''класс надпись'''
+class Label(Area):
+  def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+      self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
+  def draw(self, shift_x=0, shift_y=0):
+      self.fill()
+      window.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+
+
+
+
+
+
+
 fon = transform.scale(image.load("2.png.jpg"),(500,500))
 plat = Player('1.png.png', 5, win_width - 80,4)
 plat2 = Player('3.png.png', 435, win_width - 80,4)
 
-
+smert = transform.scale(image.load("5.png.jpg"),(250,500))
 mach = GameSprite("4.png.png",80,80,10)
-
 
 
 speed_x = 2
 speed_y = 2
 
-plat = sprite.Group()
 
-plat2 = sprite.Group()
-
-mach = sprite.Group()
-for i in range(1,2):
-    machs = GameSprite("4.png.png",80,80,10)
-    mach.add(machs)
-
+font.init()
+font = font.Font(None,35)
+lose1 = font.render('Left player lose', True, (180,10,180))
+lose2 = font.render('Right player lose', True, (10,180,180))
 run = True
 clock = time.Clock()
 FPS = 60
 while run:
-    
+
+
     for i in event.get():
         if i.type == QUIT:
             run = False
@@ -72,29 +96,39 @@ while run:
     mach.rect.x += speed_x
     mach.rect.y += speed_y
 
-
-    if mach.rect.y > win_height - 50 or mach.rect.y < 0:
-        speed_y *= -1
-        speed_x *= -1
-    if mach.rect.y > win_height - -50 or mach.rect.y < 0:
-        speed_y *= 1
-        speed_x *= 1
-    if sprite.spritecollide(plat,mach,False):
-        pass
-    if sprite.spritecollide(plat,mach2,False):
-        pass
-
-
-
-
-
-
     window.blit(fon,(0,0))
+
+
     mach.reset()
     plat.update()
     plat.reset()
     plat2.update2()
     plat2.reset()    
+#    window.blit(lose1,(100,100))
+
+
+    if mach.rect.y > win_height - 50 or mach.rect.y < 0:
+        speed_y *= -1
+        speed_x *= 1
+    if mach.rect.y > win_height - -50 or mach.rect.y < 0:
+        speed_y *= 1
+        speed_x *= 1
+    if sprite.collide_rect(plat,mach) or sprite.collide_rect(plat2,mach):
+        speed_y *= 1
+        speed_x *= -1
+
+
+    if mach.rect.x >= 500:
+        window.blit(smert,(250,0))
+        mach.rect.y = 250
+        mach.rect.x = 250
+
+
+    if mach.rect.x <= -0:
+        window.blit(smert,(250,0))
+        mach.rect.y = 250
+        mach.rect.x = 250
+        
 
     display.update()
     clock.tick(FPS)
